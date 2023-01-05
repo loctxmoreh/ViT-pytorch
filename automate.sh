26a980cb25ea7f34a53c2d4c187e79f99a0eb0d8
Tran Xuan Loc <loc.tran@moreh.com.vn>
2023-01-05 17:41:14 +0900

    automated test on nightly torch 1.10 on HAC VM

--- automate.sh
+++ automate.sh
@@ -7,19 +7,22 @@ if [[ ! -f checkpoint/ViT-B_16.npz ]]; then
     wget https://storage.googleapis.com/vit_models/imagenet21k/ViT-B_16.npz -P ./checkpoint/
 fi
 
-env_file="hacenv.yml"
+#env_file="hacenv.yml"
+env_file="hacenv2.yml"
 env_name=$(grep ^name: $env_file | awk '{ print $NF }')
 
 conda env create -f $env_file
-conda run -n $env_name update-moreh --force
+conda run -n $env_name update-moreh --force --nightly
 
 # install apex
 [[ -d apex ]] && rm -rf apex
 git clone https://github.com/NVIDIA/apex
 conda run -n $env_name pip install -v --disable-pip-version-check --no-cache-dir apex/
 
+echo "Train with single precision:"
+conda run -n $env_name ./train-single-precision.sh 2>&1 | tee single-precision.log
 
-conda run -n $env_name ./train-single-precision.sh
-conda run -n $env_name ./train-mixed-precision.sh
+echo "Train with mixed precision:"
+conda run -n $env_name ./train-mixed-precision.sh 2>&1 | tee mixed-precision.log
 
 conda env remove -n $env_name
